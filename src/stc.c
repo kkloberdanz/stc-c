1db2172d4b9d4d9bc76d778b5371b03438f39e9e
Kyle Kloberdanz <kyle.g.kloberdanz@gmail.com>
2021-09-09 16:59:08 -0500

    scientific notation used for small values

--- src/stc.c
+++ src/stc.c
@@ -1,8 +1,17 @@
+#define  _GNU_SOURCE
 #include <stdio.h>
 #include <stdlib.h>
 
 #define MAX(A, B) ((A) > (B)) ? (A) : (B)
 #define MIN(A, B) ((A) < (B)) ? (A) : (B)
+
+void conversion_error(const char * const line) {
+    fprintf(
+        stderr,
+        "failed to convert '%s' to a number, ignoring future warnings.\n",
+        line
+    );
+}
 
 int main(void) {
     char *line = NULL;
@@ -12,18 +21,26 @@ int main(void) {
     double max;
     double min;
     int first_time = 1;
+    int first_conversion_warning = 1;
     ssize_t nread = 0;
 
     while ((nread = getline(&line, &zero, stdin)) != -1) {
         double as_double;
         char *end = NULL;
 
-        lines++;
-
         as_double = strtod(line, &end);
         if (end == line) {
             /* ignore non-numerical lines */
+            if (first_conversion_warning) {
+                if (nread > 1) {
+                    line[nread - 1] = 0;
+                }
+                conversion_error(line);
+                first_conversion_warning = 0;
+            }
         } else {
+            lines++;
+
             if (first_time) {
                 first_time = 0;
                 max = as_double;
@@ -35,11 +52,19 @@ int main(void) {
         }
     }
     if (lines > 0) {
+        const double mean = sum / lines;
+        const double epsilon = 0.000001;
+        const char * const fmt_string =
+            ((mean < epsilon) || (min < epsilon) || (max < epsilon))
+            ? "lines: %lu sum: %g mean: %g max: %g min: %g\n"
+            : "lines: %lu sum: %f mean: %f max: %f min: %f\n"
+            ;
+
         printf(
-            "lines: %lu sum: %f mean: %f max: %f min: %f\n",
+            fmt_string,
             lines,
             sum,
-            sum / lines,
+            mean,
             max,
             min
         );
